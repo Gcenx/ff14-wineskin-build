4f2a563049e22d1eb2b5bdca9bc89084f63ca713
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2022-07-22 10:39:09 -0400

    install.sh: wrap WSS- commands

--- install.sh
+++ install.sh
@@ -2,17 +2,77 @@
 
 WINESKIN_TARGET_NAME="Origin.app"
 
+# Launch with unix path to open said file
+# Launch without command to load the default bat/cmd/exe
 function wineskinlauncher() {
     ${PWD}/${WINESKIN_TARGET_NAME}/Contents/MacOS/wineskinlauncher "${@}"
 }
 
+
+#### start of wineskin functions ####
+
+# Installer is calling the program
+function installer() {
+    wineskinlauncher WSS-installer "${@}"
+}
+
+# Run winecfg
+function winecfg() {
+    wineskinlauncher WSS-winecfg
+}
+
+# Run windows cmd
+function cmd() {
+    wineskinlauncher WSS-cmd
+}
+
+# Run regedit app
+function regedit() {
+    wineskinlauncher WSS-regedit
+}
+
+# Run task manager
+function taskmgr() {
+    wineskinlauncher WSS-taskmgr
+}
+
+# Run windows uninstaller
+function uninstaller() {
+    wineskinlauncher WSS-uninstaller
+}
+
+# Generate Wineskins prefix
+function wineprefixcreate() {
+    wineskinlauncher WSS-wineprefixcreate
+}
+
+# Generate Wineskins prefix without settings
+function wineprefixcreatenoregs() {
+    wineskinlauncher WSS-wineprefixcreatenoregs
+}
+
+# Run wineboot
+function wineboot() {
+    wineskinlauncher WSS-wineboot
+}
+
+# Run winetricks
+function winetricks() {
+    wineskinlauncher WSS-winetricks "${@}"
+}
+
+# Tell wineserver to kill all processes
+function wineserverkill() {
+    wineskinlauncher WSS-wineserverkill
+}
+
+# Open windows explorer menu (not a browser)
 function explorer() {
     wineskinlauncher WSS-explorer
 }
 
-function winetricks() {
-    wineskinlauncher WSS-winetricks "${@}"
-}
+#### End of wineakin functions ####
+
 
 function install_deps() {
     echo "===> - Installing Origin"
